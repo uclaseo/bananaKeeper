c92991927220a2b19975882e5e13ab5ef52e41a8
Inseok <inseok.ucla@gmail.com>
2019-04-04 14:28:52 -0700

    Simplified demo test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import { create } from 'react-test-renderer';
 import App from '../App';
-import renderer from 'react-test-renderer';
 
-test('test', () => {
-  const component = renderer.create(<App />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  
-
-})
+describe('App component', () => {
+  test('it matches the snapshot', () => {
+    const component = create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
